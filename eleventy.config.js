7a4551479857a3169d16bd32eab6dfa8996c1017
DanMakor <danielmcauliffe@outlook.com>
2026-03-25 20:48:48 +1100

    initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,63 @@
+import fs from 'fs';
+import path from 'path';
+
+import cssnano from 'cssnano';
+import postcss from 'postcss';
+import tailwindcss from '@tailwindcss/postcss';
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+export default function (eleventyConfig) {
+  //compile tailwind before eleventy processes the files
+  eleventyConfig.on('eleventy.before', async () => {
+    const tailwindInputPath = path.resolve('./src/assets/styles/index.css');
+
+    const tailwindOutputPath = './dist/assets/styles/index.css';
+
+    const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
+
+    const outputDir = path.dirname(tailwindOutputPath);
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    const result = await processor.process(cssContent, {
+      from: tailwindInputPath,
+      to: tailwindOutputPath,
+    });
+
+    fs.writeFileSync(tailwindOutputPath, result.css);
+  });
+
+  const processor = postcss([
+    //compile tailwind
+    tailwindcss(),
+
+    //minify tailwind css
+    cssnano({
+      preset: 'default',
+    }),
+  ]);
+
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    // output image formats
+    formats: ["avif", "webp", "jpeg"],
+
+    // output image widths
+    widths: ["auto"],
+
+    // optional, attributes assigned on <img> nodes override these values
+    htmlOptions: {
+        imgAttributes: {
+            loading: "lazy",
+            decoding: "async",
+        },
+        pictureAttributes: {}
+    },
+  });
+
+  eleventyConfig.addPassthroughCopy({ 'src/assets/images': 'assets/images' });
+
+  return {
+    dir: { input: 'src', output: 'dist' },
+  };
+}
